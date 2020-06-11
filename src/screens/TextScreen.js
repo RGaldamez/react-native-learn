100dcd252596c29b7132395af519b9ac37c2deac
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-06-10 21:51:05 -0600

    Changing text screen into a password demo

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,21 +2,25 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View>
-      <Text>Enter name: </Text>
+      <Text>Enter password: </Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
+        value={password}
         onChangeText={newValue => {
-          setName(newValue);
+          setPassword(newValue);
         }}
-        //value="somename"
+        //value="somepassword"
       />
-      <Text>My name is {name}</Text>
+      {password.length < 5 ? (
+        <Text>Password must be longer than 5 Characters</Text>
+      ) : (
+        <Text />
+      )}
     </View>
   );
 };
